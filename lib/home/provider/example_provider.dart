57fee45a2aa2fd1b06f5d5c1c8e1fe908dcab02e
tatarmagda <tatar.magdalena@wp.pl>
2022-02-19 12:03:26 +0100

    p2

--- lib/home/provider/example_provider.dart
+++ lib/home/provider/example_provider.dart
@@ -1,6 +1,20 @@
+import 'dart:math';
+
 import 'package:flutter/foundation.dart';
 
 class ExampleProvider extends ChangeNotifier {
   double? _heigt;
   double? _width;
+
+  _init() {
+    _heigt = Random().nextInt(350).toDouble();
+    _width = Random().nextInt(350).toDouble();
+  }
+
+  ExampleProvider() {
+    _init();
+  }
+
+  double? get heigt => _heigt;
+  double? get width => _width;
 }
